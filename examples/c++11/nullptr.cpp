b40989d93776f6166ef901b05ef4031d8abc8b95
Naum Puroski <naum.puroski@gmail.com>
2024-06-26 22:00:14 +0000

    added ctest support

--- examples/c++11/nullptr.cpp
+++ examples/c++11/nullptr.cpp
@@ -1,6 +1,19 @@
 #include <gtest/gtest.h>
 
-TEST(NullptrTest, IsSameAsNull_Not) { 
-    auto IsSameAsNull = std::is_same<decltype(NULL), decltype(nullptr)>::value; 
-    EXPECT_FALSE(IsSameAsNull);
+TEST(TestNullptr, IsSameAsNull_Not) { 
+    const auto IsSameAsNull = std::is_same<decltype(NULL), decltype(nullptr)>::value; 
+    EXPECT_FALSE(IsSameAsNull) << "decltype(nullptr) != decltype(nullptr)";
+}
+
+int foo(char*) {
+    return 1;
+}
+
+int foo(int i) {
+    return 2;
+}
+
+TEST(TestNullptr, CallTheRightAPI_NullptrCallsPtrVersion) { 
+    EXPECT_EQ(foo(nullptr), 1) << "foo(char*) is not called";
+    EXPECT_EQ(foo(0), 2) << "foo(int) is not called";
 }
