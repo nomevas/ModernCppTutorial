609f314825345a0e789895fca695e459495a9e74
Naum Puroski <naum.puroski@gmail.com>
2024-06-27 21:57:50 +0000

    adjusted test names

--- examples/c++11/nullptr.cpp
+++ examples/c++11/nullptr.cpp
@@ -1,12 +1,12 @@
 #include <gtest/gtest.h>
 
-TEST(TestNullptr, IsSameAsNull_False) { 
+TEST(Nullptr, IsSameAsNull_False) { 
     auto temp =  std::is_same_v<decltype(nullptr), std::nullptr_t>;
     const auto IsSameAsNull = std::is_same<decltype(NULL), decltype(nullptr)>::value; 
     EXPECT_FALSE(IsSameAsNull) << "decltype(nullptr) != decltype(nullptr)";
 }
 
-TEST(TestNullptr, IsSameAsStdNullptrT_True) { 
+TEST(Nullptr, IsSameAsStdNullptrT_True) { 
     const auto isSameAsNullPtrT =  std::is_same_v<decltype(nullptr), std::nullptr_t>;
     EXPECT_TRUE(isSameAsNullPtrT) << "decltype(nullptr) != std::nullptr_t";
 }
@@ -19,7 +19,7 @@ int foo(int i) {
     return 2;
 }
 
-TEST(TestNullptr, CallTheRightAPI_NullptrCallsPtrVersion) { 
+TEST(Nullptr, OverloadedMethods_NullptrCallsPtrFlavor) { 
     EXPECT_EQ(foo(nullptr), 1) << "foo(char*) is not called";
     EXPECT_EQ(foo(0), 2) << "foo(int) is not called";
 }
