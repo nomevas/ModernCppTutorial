8ae515a57dd8997f6b3cbd165447c19a029461dd
Naum Puroski <naum.puroski@gmail.com>
2024-10-07 22:04:14 +0000

    Added Array (Matrix) factory using constexpr Fill method

--- README.md
+++ README.md
@@ -3,6 +3,12 @@ Exploring modern c++ features using unit tests.
 
 upcoming:
 std::expected
+-> monadic extension
 std::variant
+-> https://www.cppstories.com/2023/finite-state-machines-variant-cpp/
 std::optional
+co-rutines
+-> streams...
+
+unamed namespaces...
 ...
--- examples/c++11/array.cpp
+++ examples/c++11/array.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
 #include <algorithm>
 #include <array>
@@ -7,6 +8,10 @@
 #include <unordered_map>
 #include <unordered_set>
 
+using testing::Each;
+using testing::AllOf;
+using testing::Eq;
+
 TEST(Array, FixedSizeContainer_AvoidPerformanceSpaceOverhead) {
   std::array<int, 4> arr = {1, 2, 3, 4};
 
@@ -14,4 +19,32 @@ TEST(Array, FixedSizeContainer_AvoidPerformanceSpaceOverhead) {
   EXPECT_FALSE(arr.empty());
   std::ranges::sort(arr, std::greater<int>());
   EXPECT_EQ(4, arr[0]);
+}
+
+template <typename T, int N> 
+constexpr auto MakeArray(const T& default_value) {
+  std::array<T, N> array;
+  array.fill(default_value); // constexpr since C++20 
+  return array;
+}
+
+template <typename T, int N, int M> 
+constexpr auto MakeMatrix(const T& default_value) {
+  auto array = MakeArray<T, N>(default_value);
+  std::array<decltype(array), M> matrix;
+  matrix.fill(array); // constexpr since C++20 
+  return matrix;
+}
+
+TEST(Array, CreateArrayWithDefaultValue) {
+  auto array = MakeArray<int, 10>(-1);
+  EXPECT_EQ(10, array.size());
+  ASSERT_THAT(array, Each(Eq(-1)));
+}
+
+TEST(Array, CreateMatrixWithDefaultValue) {
+  auto matrix = MakeMatrix<int, 10, 10>(-1);
+  EXPECT_EQ(10, matrix.size());
+  EXPECT_EQ(10, matrix[0].size());
+  ASSERT_THAT(matrix, Each(Eq(MakeArray<int, 10>(-1))));
 }
